bbfe31702032f0bad9a0239fd0a32785bc7a7f9b
chris0512 <kyang3200@gmail.com>
2022-03-29 17:01:32 +0800

    show the find method

--- sandbox.js
+++ sandbox.js
@@ -1,34 +1,5 @@
-const scores = [10, 20, 60, 40, 70, 90, 30];
+const scores = [10, 5, 0, 40, 30, 10, 90, 70];
 
-// const result = scores.reduce((acc, curr)=>{
-//     if(curr > 50){
-//         acc++;
-//     }
-//     return acc;
-// }, 0);
+const firstHighScore = scores.find(score=>score > 50);
 
-// console.log(result);
-
-const score = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60},
-    {player: 'mario', score: 90},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60},
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60},
-];
-
-const maroiTotal = score.reduce((acc, curr) => {
-    if(curr.player === 'mario'){
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
-
-console.log(maroiTotal);
+console.log(firstHighScore);
